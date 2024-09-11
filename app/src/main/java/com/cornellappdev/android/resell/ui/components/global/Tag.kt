7bd474fee1138d16c514f6fb760d2c53719e60ed
thisjustin123 <jjg283@cornell.edu>
2024-09-11 14:54:14 -0400

    Make tag toggleable and animated

--- app/src/main/java/com/cornellappdev/android/resell/ui/components/global/Tag.kt
+++ app/src/main/java/com/cornellappdev/android/resell/ui/components/global/Tag.kt
@@ -1,7 +1,7 @@
 package com.cornellappdev.android.resell.ui.components.global
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
@@ -9,14 +9,16 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.cornellappdev.android.resell.ui.theme.ResellGradientDiagonal
-import com.cornellappdev.android.resell.ui.theme.Stroke
 import com.cornellappdev.android.resell.ui.theme.Style
+import com.cornellappdev.android.resell.ui.theme.animateResellBrush
 
 @Composable
 fun Tag(
@@ -25,17 +27,14 @@ fun Tag(
     active: Boolean = false,
     onClick: () -> Unit,
 ) {
-    val border = if (active) {
-        BorderStroke(
-            width = 2.dp,
-            brush = ResellGradientDiagonal,
-        )
-    } else {
-        BorderStroke(
-            width = 1.dp,
-            brush = SolidColor(Stroke),
-        )
-    }
+    val borderWidth = animateFloatAsState(
+        targetValue = if (active) 2f else 1f,
+        label = "tag border width"
+    )
+    val border = BorderStroke(
+        width = borderWidth.value.dp,
+        brush = animateResellBrush(active)
+    )
 
     Surface(
         onClick = onClick,
@@ -55,6 +54,7 @@ fun Tag(
 @Preview
 @Composable
 private fun TagPreview() {
+    var toggle by remember { mutableStateOf(true) }
     Column(modifier = Modifier.padding(8.dp)) {
         Tag(
             text = "active",
@@ -69,6 +69,13 @@ private fun TagPreview() {
             active = false,
             onClick = {}
         )
-    }
 
+        Spacer(modifier = Modifier.padding(8.dp))
+
+        Tag(
+            text = "toggleable",
+            active = toggle,
+            onClick = { toggle = !toggle }
+        )
+    }
 }
