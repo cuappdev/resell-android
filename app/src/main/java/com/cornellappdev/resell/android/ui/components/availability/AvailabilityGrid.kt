7a1d2d33e01ff0836b5f71ebdb625f3b26c2d905
zachseidner1 <zachary.seidner@gmail.com>
2024-10-19 17:47:01 -0400

    Finish availability grid

--- app/src/main/java/com/cornellappdev/resell/android/ui/components/availability/AvailabilityGrid.kt
+++ app/src/main/java/com/cornellappdev/resell/android/ui/components/availability/AvailabilityGrid.kt
@@ -2,12 +2,20 @@ package com.cornellappdev.resell.android.ui.components.availability
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.gestures.calculateCentroid
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
@@ -22,15 +30,140 @@ import androidx.compose.ui.unit.toSize
 import com.cornellappdev.resell.android.ui.theme.ResellPreview
 import com.cornellappdev.resell.android.ui.theme.ResellPurple
 import com.cornellappdev.resell.android.ui.theme.Stroke
+import com.cornellappdev.resell.android.ui.theme.Style
 import com.cornellappdev.resell.android.ui.theme.changeBrightness
 import com.cornellappdev.resell.android.util.toSortedPair
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 import kotlin.math.floor
+
+@Preview
+@Composable
+private fun AvailabilityGrid_RUNME_Preview() = ResellPreview {
+    var selectedAvailabilities by remember { mutableStateOf(emptyList<LocalDateTime>()) }
+    Column {
+        Text(
+            "Selected availabilities: ${
+                selectedAvailabilities
+                    .joinToString(", ")
+                    { it.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) }
+            }",
+            style = Style.body1
+        )
+        AvailabilityGrid(
+            listOf(
+                LocalDateTime.now().minusDays(1L),
+                LocalDateTime.now(),
+                LocalDateTime.now().plusDays(1L)
+            ),
+            setSelectedAvailabilities = {
+                selectedAvailabilities = it
+            },
+            modifier = Modifier.weight(1F)
+        )
+    }
+}
+
+@Composable
+fun AvailabilityGrid(
+    dates: List<LocalDateTime>,
+    setSelectedAvailabilities: (List<LocalDateTime>) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val width = dates.size
+    val height = 24
+    val dayStart = LocalTime.of(9, 0)
+
+    var grid by remember {
+        mutableStateOf(
+            buildList {
+                repeat(height) {
+                    add(BooleanArray(width))
+                }
+            }
+        )
+    }
+
+    fun List<BooleanArray>.toAvailabilities(): List<LocalDateTime> =
+        flatMapIndexed { row, cells ->
+            cells.mapIndexed { col, filled ->
+                if (!filled) {
+                    null
+                } else {
+                    val day = dates[col]
+                    day
+                        .withHour(dayStart.hour)
+                        .withMinute(dayStart.minute)
+                        .withSecond(dayStart.second)
+                        .withNano(dayStart.nano)
+                        .plusMinutes(30L * row)
+                }
+            }.filterNotNull()
+        }
+
+
+    fun getTimeForRow(row: Int): LocalTime {
+        return dayStart.plusHours(1L * row)
+    }
+
+    Column(modifier = modifier) {
+        Box(modifier = Modifier.weight(1F)) {
+            Row {
+                Spacer(modifier = Modifier.weight(1F))
+                Row(
+                    modifier = Modifier
+                        .weight(3F)
+                        .fillMaxHeight(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceAround,
+                ) {
+                    dates.forEach {
+                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                            Text(
+                                it.format(DateTimeFormatter.ofPattern("E")),
+                                style = Style.title1
+                            )
+                            Text(
+                                it.format(DateTimeFormatter.ofPattern("MMM d")),
+                                style = Style.body1
+                            )
+                        }
+                    }
+                }
+            }
+        }
+        Row(modifier = Modifier.weight(height.toFloat() / 2)) {
+            Column(
+                modifier = Modifier
+                    .weight(1F)
+                    .fillMaxHeight(),
+                verticalArrangement = Arrangement.SpaceAround,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                repeat(height / 2) { index ->
+                    Text(
+                        getTimeForRow(index).format(DateTimeFormatter.ofPattern("hh:mm a")),
+                        style = Style.title2
+                    )
+                }
+            }
+            Box(modifier = Modifier.weight(width.toFloat())) {
+                SelectableGrid(grid = grid, updateGrid = {
+                    val newGrid = it(grid)
+                    grid = newGrid
+                    setSelectedAvailabilities(newGrid.toAvailabilities())
+                })
+            }
+        }
+    }
+}
 
 /**
  * Requires: grid is non-empty
  */
 @Composable
-fun SelectableGrid(
+private fun SelectableGrid(
     grid: List<BooleanArray>,
     updateGrid: ((List<BooleanArray>) -> List<BooleanArray>) -> Unit,
     gridStroke: Color = Stroke,
@@ -43,8 +176,8 @@ fun SelectableGrid(
     var selectionEnd by remember { mutableStateOf<Pair<Int, Int>?>(null) }
 
     fun getGridCell(offset: Offset, canvasSize: Size): Pair<Int, Int> {
-        val gridCol = floor(offset.x / (canvasSize.width / width)).toInt()
-        val gridRow = floor(offset.y / (canvasSize.height / height)).toInt()
+        val gridCol = floor(offset.x / (canvasSize.width / width)).toInt().coerceIn(0, width - 1)
+        val gridRow = floor(offset.y / (canvasSize.height / height)).toInt().coerceIn(0, height - 1)
         return gridRow to gridCol
     }
 
@@ -99,21 +232,35 @@ fun SelectableGrid(
         }) {
         val rectWidth = size.width / width
         val rectHeight = size.height / height
+        /**
+         * We don't want to subdivide the grid too much vertically or it will be overwhelming
+         * for the user. So we only show half of the vertical grid cells, unless they are filled
+         * in.
+         */
+        for (row in grid.indices.filter { it % 2 == 0 }) {
+            for (col in grid[row].indices) {
+                val position = Offset(rectWidth * col, rectHeight * row)
 
+                drawRect(
+                    size = Size(rectWidth, rectHeight * 2),
+                    topLeft = position,
+                    color = gridStroke,
+                    style = Stroke(width = 4F)
+                )
+            }
+        }
         for (row in grid.indices) {
             for (col in grid[row].indices) {
-                val style = if (grid[row][col]) {
-                    Fill
-                } else {
-                    Stroke(width = 4f)
+                if (grid[row][col]) {
+                    val position = Offset(rectWidth * col, rectHeight * row)
+
+                    drawRect(
+                        size = Size(rectWidth, rectHeight),
+                        topLeft = position,
+                        color = fillColor,
+                        style = Fill
+                    )
                 }
-                val position = Offset(rectWidth * col, rectHeight * row)
-                drawRect(
-                    size = Size(rectWidth, rectHeight),
-                    topLeft = position,
-                    color = if (grid[row][col]) fillColor else gridStroke,
-                    style = style
-                )
             }
         }
         selectionStart?.let { selectionStart ->
