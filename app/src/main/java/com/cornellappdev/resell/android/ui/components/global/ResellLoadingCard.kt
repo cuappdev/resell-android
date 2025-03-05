79f4096ff364eb7cd0f2f52d24f71562fc5d96d6
Caleb Shim <chs232@cornell.edu>
2025-03-05 18:07:59 -0500

    Addressed PR comments

--- app/src/main/java/com/cornellappdev/resell/android/ui/components/global/ResellLoadingCard.kt
+++ app/src/main/java/com/cornellappdev/resell/android/ui/components/global/ResellLoadingCard.kt
@@ -2,14 +2,14 @@ package com.cornellappdev.resell.android.ui.components.global
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
@@ -19,11 +19,10 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.times
+import com.cornellappdev.resell.android.ui.theme.ResellPreview
 import com.cornellappdev.resell.android.ui.theme.Stroke
-import com.cornellappdev.resell.android.ui.theme.Wash
 import com.cornellappdev.resell.android.util.shimmer
 
 @Composable
@@ -32,65 +31,55 @@ fun ResellLoadingCard(
     small: Boolean
 ) {
     val screenWidth = LocalConfiguration.current.screenWidthDp.dp
-    val width = 0.5f * screenWidth
-    val scale = 1.0 / 173.0 * width
-    val height = if (small) 144.73 * scale else 246.4 * scale
-    val contentHeight = if(small) 111.73*scale else 213.4 * scale
+    val maxWidth = 0.5f * screenWidth
+    val contentHeight = if (small) 112.dp else 213.dp
     Column(
         modifier = modifier
-            .widthIn(max = width)
+            .widthIn(max = maxWidth)
             .fillMaxWidth()
             .clip(RoundedCornerShape(size = 8.dp))
             .background(Color.White)
             .border(width = 1.dp, color = Stroke, shape = RoundedCornerShape(8.dp))
-            .heightIn(max = height)
     ) {
         Box(
-            modifier = modifier
-                .heightIn(max = contentHeight)
-                .fillMaxSize()
-                .background(
-                    Wash
-                )
-//                .shimmer()
+            modifier = Modifier
+                .height(contentHeight)
+                .fillMaxWidth()
+                .shimmer()
         )
 
         Row(
             modifier = Modifier
-                .fillMaxSize(),
-            horizontalArrangement = Arrangement.SpaceEvenly,
+                .fillMaxWidth()
+                .padding(8.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            LoadingText(106.0f, scale)
-            LoadingText(45.0f, scale)
+            LoadingText(Modifier.weight(1f))
+            Spacer(Modifier.width(6.dp))
+            LoadingText(Modifier.width(45.dp))
         }
     }
 }
 
 @Composable
-private fun LoadingText(width: Float, scale: Dp) {
+private fun LoadingText(modifier: Modifier) {
     Column(
-        modifier = Modifier
-            .widthIn(max = width * scale)
-            .heightIn(max = 17.0 * scale)
-            .fillMaxWidth()
+        modifier = modifier
+            .height(17.dp)
             .clip(RoundedCornerShape(size = 100.dp))
-            .background(Wash)
-//            .shimmer()
+            .shimmer()
             .border(width = 3.dp, color = Color.White, shape = RoundedCornerShape(100.dp))
-            .fillMaxHeight()
     ) { }
-
 }
 
 @Preview
 @Composable
-private fun PreviewResellSmallLoadingCard() {
+private fun PreviewResellSmallLoadingCard() = ResellPreview {
     ResellLoadingCard(modifier = Modifier, small = true)
 }
 
 @Preview
 @Composable
-private fun PreviewResellBigLoadingCard() {
+private fun PreviewResellBigLoadingCard() = ResellPreview {
     ResellLoadingCard(modifier = Modifier, small = false)
 }
