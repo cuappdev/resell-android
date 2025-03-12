6a1ceb777e9a6886a6ca017ac7b48ab7f8389a07
Caleb Shim <chs232@cornell.edu>
2025-03-12 18:06:20 -0400

    Randomize cards, implement infinite scroll.

--- app/src/main/java/com/cornellappdev/resell/android/ui/components/global/ResellLoadingListingScroll.kt
+++ app/src/main/java/com/cornellappdev/resell/android/ui/components/global/ResellLoadingListingScroll.kt
@@ -1,37 +1,45 @@
 package com.cornellappdev.resell.android.ui.components.global
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.staggeredgrid.LazyStaggeredGridState
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.rememberLazyStaggeredGridState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cornellappdev.resell.android.ui.theme.ResellPreview
+import kotlin.random.Random
 
 @Composable
 fun ResellLoadingListingScroll(
     modifier: Modifier = Modifier,
-    numCards: Int = 5,
+    numCards: Int = Int.MAX_VALUE,
     listState: LazyStaggeredGridState = rememberLazyStaggeredGridState(),
 ) {
+    val randomList by remember {
+        mutableStateOf(List(50) { i ->
+            when (i) {
+                0 -> true
+                1 -> false
+                else -> Random.nextBoolean()
+            }
+        })
+    }
+
     LazyVerticalStaggeredGrid(
         columns = StaggeredGridCells.Fixed(2),
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier,
         state = listState,
         verticalItemSpacing = 24.dp,
         horizontalArrangement = Arrangement.spacedBy(20.dp)
     ) {
-        if (numCards >= 1) {
-            item {
-                ResellLoadingCard(small = true)
-            }
-            items(numCards - 1) {
-                ResellLoadingCard(small = false)
-            }
+        items(numCards) { idx ->
+            ResellLoadingCard(small = randomList[idx % randomList.size])
         }
     }
 }
@@ -39,7 +47,7 @@ fun ResellLoadingListingScroll(
 @Preview
 @Composable
 private fun PreviewHomeLoadingScroll() = ResellPreview {
-    ResellLoadingListingScroll(numCards = 5)
+    ResellLoadingListingScroll()
 }
 
 
