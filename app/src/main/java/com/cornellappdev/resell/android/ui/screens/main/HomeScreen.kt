b266f975c86ec8a26aea8157498e7e41462106f7
Caleb Shim <chs232@cornell.edu>
2025-03-12 18:26:19 -0400

    Pass in padding

--- app/src/main/java/com/cornellappdev/resell/android/ui/screens/main/HomeScreen.kt
+++ app/src/main/java/com/cornellappdev/resell/android/ui/screens/main/HomeScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.windowInsetsPadding
@@ -74,7 +75,7 @@ fun HomeScreen(
             }
 
             is ResellApiState.Loading -> {
-                ResellLoadingListingScroll()
+                ResellLoadingListingScroll(modifier = Modifier.padding(horizontal = 6.dp))
             }
 
             is ResellApiState.Error -> {}
