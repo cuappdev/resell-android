119b46e480c74ac0fbc50c535f49b4f2cfb5a13f
Caleb Shim <chs232@cornell.edu>
2025-04-22 11:14:21 -0400

    Remove nested row, use showBackground

--- app/src/main/java/com/cornellappdev/resell/android/ui/components/main/SearchBar.kt
+++ app/src/main/java/com/cornellappdev/resell/android/ui/components/main/SearchBar.kt
@@ -3,8 +3,8 @@ package com.cornellappdev.resell.android.ui.components.main
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -14,7 +14,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,34 +28,28 @@ fun SearchBar(
     modifier: Modifier = Modifier,
 ) {
     Row(
-        modifier = modifier.clickable(onClick = onClick),
-        horizontalArrangement = Arrangement.spacedBy(12.dp)
-    ) {
-        Row(
-            modifier = Modifier
-                .weight(1f)
-                .height(43.dp)
-                .clip(RoundedCornerShape(40.dp))
-                .background(color = Wash)
-                .padding(start = 15.dp, top = 10.dp, bottom = 9.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(12.dp),
+        modifier = modifier
+            .fillMaxWidth()
+            .height(43.dp)
+            .clip(RoundedCornerShape(40.dp))
+            .background(color = Wash)
+            .padding(start = 15.dp, top = 10.dp, bottom = 9.dp)
+            .clickable(onClick = onClick),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
 
-            ) {
-            Icon(
-                painter = painterResource(R.drawable.ic_search),
-                contentDescription = "Search Icon",
-                tint = Secondary
-            )
-            Text(text = "Search", style = Style.body2, color = Secondary)
-        }
+        ) {
+        Icon(
+            painter = painterResource(R.drawable.ic_search),
+            contentDescription = "Search Icon",
+            tint = Secondary
+        )
+        Text(text = "Search", style = Style.body2, color = Secondary)
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun SearchBarPreview() {
-    Box(modifier = Modifier.background(color = Color.White)) {
-        SearchBar(onClick = {})
-    }
+    SearchBar(onClick = {})
 }
