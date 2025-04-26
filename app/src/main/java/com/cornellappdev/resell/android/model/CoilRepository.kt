33030c6784629609a116ec65a91d24ba87a38e7f
zachseidner1 <zachary.seidner@gmail.com>
2025-04-26 13:52:43 -0400

    Start refactor

--- app/src/main/java/com/cornellappdev/resell/android/model/CoilRepository.kt
+++ app/src/main/java/com/cornellappdev/resell/android/model/CoilRepository.kt
@@ -2,7 +2,6 @@ package com.cornellappdev.resell.android.model
 
 import android.content.Context
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.core.graphics.drawable.toBitmap
@@ -12,6 +11,10 @@ import com.cornellappdev.resell.android.model.classes.ResellApiResponse
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -24,8 +27,8 @@ class CoilRepository @Inject constructor(
     @ApplicationContext private val context: Context
 ) {
     // TODO Make sure this doesn't cause OOM with too many images. Maybe put a size limit?
-    private val urlMap: MutableMap<String, MutableState<ResellApiResponse<ImageBitmap>>> =
-        mutableMapOf()
+    private val _urlMapFlow =
+        MutableStateFlow<Map<String, ResellApiResponse<ImageBitmap>>>(emptyMap<String, ResellApiResponse<ImageBitmap>>().withDefault { ResellApiResponse.Pending })
 
     /**
      * Returns a [MutableState] containing an [ResellApiResponse] corresponding to a loading or loaded
@@ -34,30 +37,49 @@ class CoilRepository @Inject constructor(
      *
      * Loads images with Coil.
      */
-    fun getUrlState(imageUrl: String): MutableState<ResellApiResponse<ImageBitmap>> {
+    fun getUrlState(imageUrl: String): Flow<ResellApiResponse<ImageBitmap>> {
+        requestImageUrl(imageUrl)
+
+        return _urlMapFlow.map { urlMap ->
+            return@map urlMap.getValue(imageUrl)
+        }
+    }
+
+    fun getImageUrlStates(imageUrls: List<String>): Flow<List<ResellApiResponse<ImageBitmap>>> {
+        TODO("Someone could implement this, eventually. It should follow pretty easily from the previous implementation")
+    }
+
+    /**
+     * Given an image URL, requests the image bitmap, and updates _urlMapFlow accordingly
+     * If the image bitmap request was already successful, this function does nothing
+     */
+    private fun requestImageUrl(imageUrl: String) {
         val imageLoader = context.imageLoader
 
-        // Make new request.
-        if (!urlMap.containsKey(imageUrl) || urlMap[imageUrl]!!.value is ResellApiResponse.Error) {
-            val imageRequest = ImageRequest.Builder(context)
-                .data(imageUrl)
-                .build()
+        if (_urlMapFlow.value.getValue(imageUrl) is ResellApiResponse.Success) {
+            return
+        }
 
-            urlMap[imageUrl] = mutableStateOf(ResellApiResponse.Pending)
+        // Make the request
+        val imageRequest = ImageRequest.Builder(context)
+            .data(imageUrl)
+            .build()
 
-            val disposable = imageLoader.enqueue(imageRequest)
+        val disposable = imageLoader.enqueue(imageRequest)
 
-            CoroutineScope(Dispatchers.IO).launch {
-                val result = disposable.job.await()
-                if (result.drawable == null) {
-                    urlMap[imageUrl]!!.value = ResellApiResponse.Error
-                } else {
-                    urlMap[imageUrl]!!.value =
-                        ResellApiResponse.Success(result.drawable!!.toBitmap().asImageBitmap())
+        CoroutineScope(Dispatchers.IO).launch {
+            val result = disposable.job.await()
+            if (result.drawable == null) {
+                _urlMapFlow.update {
+                    it + (imageUrl to ResellApiResponse.Error)
+                }
+            } else {
+                _urlMapFlow.update {
+                    it + (imageUrl to ResellApiResponse.Success(
+                        result.drawable!!.toBitmap().asImageBitmap()
+                    ))
                 }
             }
         }
-
-        return urlMap[imageUrl]!!
     }
 }
