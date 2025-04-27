ccb47d71e41a565e911dc76e46134861a5a9a8f3
Caleb Shim <chs232@cornell.edu>
2025-04-26 23:23:43 -0400

    Safe list access

--- app/src/main/java/com/cornellappdev/resell/android/ui/screens/main/HomeScreen.kt
+++ app/src/main/java/com/cornellappdev/resell/android/ui/screens/main/HomeScreen.kt
@@ -33,6 +33,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -274,9 +276,12 @@ private fun SavedListingsRow(
                         .clip(RoundedCornerShape(4.dp))
                 )
             } else {
+                val image: MutableState<ResellApiResponse<ImageBitmap>> = remember {
+                    savedImageResponses.getOrNull(index) ?: mutableStateOf(ResellApiResponse.Error)
+                }
                 ResellSavedCard(
                     onClick = { toPost(listing) },
-                    imageResponse = savedImageResponses[index]
+                    imageResponse = image
                 )
             }
         }
