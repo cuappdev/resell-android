196c86bbf9bf0c1a6c03ea3b0bb3dbf526b59653
Caleb Shim <chs232@cornell.edu>
2025-04-27 17:52:33 -0400

    Add FilterBottomSheet UI

---
+++ app/src/main/java/com/cornellappdev/resell/android/ui/components/main/FilterBottomSheet.kt
@@ -0,0 +1,366 @@
+package com.cornellappdev.resell.android.ui.components.main
+
+//noinspection UsingMaterialAndMaterial3Libraries
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.RangeSlider
+import androidx.compose.material.SliderDefaults
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchColors
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.cornellappdev.resell.android.R
+import com.cornellappdev.resell.android.ui.theme.IconInactive
+import com.cornellappdev.resell.android.ui.theme.PurpleWash
+import com.cornellappdev.resell.android.ui.theme.ResellPurple
+import com.cornellappdev.resell.android.ui.theme.ResellTheme
+import com.cornellappdev.resell.android.ui.theme.Secondary
+import com.cornellappdev.resell.android.ui.theme.Stroke
+import com.cornellappdev.resell.android.ui.theme.Style
+import com.cornellappdev.resell.android.viewmodel.main.HomeViewModel.Category
+import com.cornellappdev.resell.android.viewmodel.main.HomeViewModel.Condition
+import com.cornellappdev.resell.android.viewmodel.main.HomeViewModel.ResellFilter
+
+@Composable
+fun FilterBottomSheet(
+    filter: ResellFilter,
+    onFilterChanged: (ResellFilter) -> Unit,
+    bottomPadding: Dp
+) {
+    val categoriesSelectedCurrent = remember { filter.categoriesSelected }
+    val conditionsSelectedCurrent = remember { filter.conditionsSelected }
+    val itemsOnSaleCurrent = remember { mutableStateOf(filter.itemsOnSale) }
+    val priceRange = remember { mutableStateOf(filter.priceRange) }
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 36.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        item {
+            Text(
+                text = "Filters",
+                style = Style.heading2,
+                modifier = Modifier.padding(bottom = 20.dp)
+            )
+        }
+        item {
+            HorizontalDivider(modifier = Modifier.fillMaxWidth(), thickness = 1.dp, color = Stroke)
+        }
+        item {
+            Spacer(modifier = Modifier.height(36.dp))
+        }
+        item {
+            AllFilters(
+                itemsOnSaleCurrent,
+                categoriesSelectedCurrent,
+                conditionsSelectedCurrent,
+                priceRange,
+                onFilterChanged
+            )
+        }
+        item {
+            Spacer(modifier = Modifier.height(bottomPadding))
+        }
+    }
+}
+
+@Composable
+@OptIn(
+    ExperimentalLayoutApi::class, ExperimentalMaterial3Api::class,
+    ExperimentalMaterialApi::class
+)
+private fun AllFilters(
+    itemsOnSale: MutableState<Boolean>,
+    categoriesSelected: MutableMap<Category, Boolean>,
+    conditionsSelected: MutableMap<Condition, Boolean>,
+    priceRange: MutableState<ClosedFloatingPointRange<Float>>,
+    onFilterChanged: (ResellFilter) -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 24.dp)
+            .fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text("Sort by", style = Style.heading3)
+            Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                Text(
+                    text = "Any",
+                    color = Secondary,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 20.sp
+                )
+                Icon(
+                    painter = painterResource(R.drawable.baseline_keyboard_arrow_down_24),
+                    tint = Secondary,
+                    contentDescription = "Down Arrow",
+                    modifier = Modifier.clickable {
+                        //todo
+                    }
+                )
+            }
+        }
+        HorizontalDivider(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 24.dp),
+            thickness = 1.dp,
+            color = Stroke
+        )
+        Text(
+            text = "Price Range",
+            style = Style.heading3,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Text(
+            text = "Any",
+            color = Secondary,
+            fontWeight = FontWeight.Normal,
+            fontSize = 20.sp
+        )
+        // Using Material 2 RangeSlider to match design
+        RangeSlider(
+            value = priceRange.value,
+            onValueChange = { range -> priceRange.value = range },
+            valueRange = 0f..100f,
+            colors = SliderDefaults.colors(
+                thumbColor = Color.White,
+                activeTrackColor = ResellPurple,
+                inactiveTrackColor = ResellPurple.copy(alpha = 0.2f),
+                activeTickColor = ResellPurple,
+                inactiveTickColor = ResellPurple.copy(alpha = 0.2f)
+            )
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text("\$0", style = Style.title4, color = Secondary)
+            Text("\$10000", style = Style.title4, color = Secondary)
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Items on Sale",
+                fontWeight = FontWeight.Normal,
+                fontSize = 20.sp,
+                color = Secondary
+            )
+            Switch(
+                checked = itemsOnSale.value,
+                onCheckedChange = { itemsOnSale.value = !itemsOnSale.value },
+                colors = SwitchColors(
+                    checkedThumbColor = Color.White,
+                    checkedTrackColor = ResellPurple,
+                    checkedBorderColor = ResellPurple,
+                    checkedIconColor = Color.White,
+
+                    uncheckedThumbColor = IconInactive,
+                    uncheckedTrackColor = Color.White,
+                    uncheckedBorderColor = IconInactive,
+                    uncheckedIconColor = IconInactive,
+
+                    disabledCheckedThumbColor = Color.Gray,
+                    disabledCheckedTrackColor = Color.Gray,
+                    disabledCheckedBorderColor = Color.Gray,
+                    disabledCheckedIconColor = Color.Gray,
+                    disabledUncheckedThumbColor = Color.Gray,
+                    disabledUncheckedTrackColor = Color.Gray,
+                    disabledUncheckedBorderColor = Color.Gray,
+                    disabledUncheckedIconColor = Color.Gray
+                ),
+                modifier = Modifier.scale(0.8f)
+            )
+        }
+        HorizontalDivider(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 24.dp),
+            thickness = 1.dp,
+            color = Stroke
+        )
+        Text(
+            text = "Product Category",
+            style = Style.heading3,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        FlowRow(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+        ) {
+            Category.entries.forEach { item ->
+                ResellChip(
+                    selected = categoriesSelected[item] ?: false,
+                    onClick = {
+                        categoriesSelected[item] = !(categoriesSelected[item] ?: true)
+                    },
+                    labelText = item.label
+                )
+            }
+        }
+        HorizontalDivider(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 24.dp),
+            thickness = 1.dp,
+            color = Stroke
+        )
+        Text(
+            text = "Condition",
+            style = Style.heading3,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        FlowRow(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+        ) {
+            Condition.entries.forEach { condition ->
+                ResellChip(
+                    selected = conditionsSelected[condition] ?: false,
+                    onClick = {
+                        conditionsSelected[condition] =
+                            !(conditionsSelected[condition] ?: true)
+                    },
+                    labelText = condition.label
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(36.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = "Reset", style = Style.title1, modifier = Modifier.clickable {
+                Category.entries.forEach { item ->
+                    categoriesSelected[item] = false
+                }
+                Condition.entries.forEach { condition ->
+                    conditionsSelected[condition] = false
+                }
+                itemsOnSale.value = false
+                priceRange.value = 0f..100f
+            })
+            Button(onClick = {
+                onFilterChanged(
+                    ResellFilter(
+                        priceRange = priceRange.value,
+                        itemsOnSale = itemsOnSale.value,
+                        categoriesSelected = categoriesSelected,
+                        conditionsSelected = conditionsSelected
+                    )
+                )
+            }) {
+                Text(text = "Apply Filters", style = Style.title1, color = Color.White)
+            }
+        }
+
+    }
+}
+
+@Composable
+private fun ResellChip(selected: Boolean, onClick: () -> Unit, labelText: String) {
+    FilterChip(
+        selected = selected,
+        onClick = onClick,
+        label = {
+            Text(
+                text = labelText,
+                fontWeight = FontWeight.Normal,
+                fontSize = 14.sp,
+            )
+        },
+        shape = RoundedCornerShape(100.dp),
+        border = BorderStroke(
+            width = 1.dp,
+            color = if (selected) ResellPurple else Stroke
+        ),
+        colors = if (!selected) {
+            FilterChipDefaults.filterChipColors()
+        } else {
+            FilterChipDefaults.filterChipColors(
+                labelColor = ResellPurple,
+                disabledContainerColor = Color.Unspecified,
+                disabledLabelColor = Color.Black,
+                selectedContainerColor = PurpleWash,
+                selectedLabelColor = ResellPurple,
+            )
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+private fun PreviewFilterBottomSheet() {
+    ResellTheme {
+        BottomSheetScaffold(
+            sheetContent = {
+                FilterBottomSheet(
+                    ResellFilter(
+                        itemsOnSale = false,
+                        categoriesSelected = mutableMapOf(
+                            Category.CLOTHING to false,
+                            Category.BOOKS to true,
+                            Category.SCHOOL to false,
+                            Category.ELECTRONICS to true,
+                            Category.SPORTS to false,
+                            Category.HANDMADE to false,
+                            Category.OTHER to true
+                        ),
+                        conditionsSelected = mutableMapOf(
+                            Condition.GENTLY_USED to false,
+                            Condition.NEVER_USED to true,
+                            Condition.WORN to false
+                        ),
+                        priceRange = 0f..100f
+                    ),
+                    onFilterChanged = {}, bottomPadding = 36.dp
+                )
+            },
+            sheetPeekHeight = 750.dp
+        ) {
+        }
+    }
+}
--- app/src/main/java/com/cornellappdev/resell/android/ui/theme/Color.kt
+++ app/src/main/java/com/cornellappdev/resell/android/ui/theme/Color.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
+val PurpleWash = Color(0xFFF7F2FF)
 val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
